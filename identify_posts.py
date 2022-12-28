ba3dcabb84c549493a24f92631e7f29a049e2f10
kimiGee <kimberlyreneegonzales@gmail.com>
2022-12-27 21:33:23 -0500

    updated search terms

--- identify_posts.py
+++ identify_posts.py
@@ -15,18 +15,9 @@ for city in cities:
     # each city has approxiamtely 1800 pages for the "cars for sale by owner" category
     page_number = 1
     # this while loop cycles through all 1800 pages
-    while(page_number <= 1800) and (wash_number < 5):
+    while(page_number <= 1800):
         # city_link variable takes a a different city name from the cities every time through the loop
-        
-        # washington D.C.'s listing page had a different format than the other cities, so is dealt with differently        
-        if((city.startswith(washington)) and (wash_number <= 6)):
-            for post_num in range(0, 100):
-                city_link = "https://washingtondc.craigslist.org/search/nva/cta?purveyor=owner#search=1~gallery~" + str(wash_number) + "~" + str(post_num)
-                print(city_link)
-            wash_number+=1
-        else:
-            city_link = str(city) +"/search/cta?purveyor=owner&s=" + str(page_number)
-        # we                         
+        city_link = str(city) +"/search/cta?s=" + str(page_number)                  
         list_of_cities.append(city_link)
         page_number +=120
         
@@ -49,10 +40,11 @@ for each_city_page in list_of_cities:
             # get all the html links in the page and append them to a list
             for link in posts:
                 l = link.get('href')
+                print(l)
                 city_links.append(l)                                
         except:
             pass                            
         
-
+print(len(city_links))
 df = pd.DataFrame(city_links)
 df.to_csv("./links.csv", sep=',',index=False)
